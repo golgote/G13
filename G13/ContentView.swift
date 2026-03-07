0ce0817ce808b9b6ec6acf933ae5eaf8fe5a0969
Bertrand Mansion <bmansion@mamasam.com>
2026-03-07 21:33:26 +0100

    Initial Commit

---
+++ G13/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  G13
+//
+//  Created by Bertrand Mansion on 07/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ G13/G13App.swift
@@ -0,0 +1,17 @@
+//
+//  G13App.swift
+//  G13
+//
+//  Created by Bertrand Mansion on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct G13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
